bedf5e6a600e8898a69acead9ee6cfae032e55b9
Swarleen Bhamra <80758163+Swarleen@users.noreply.github.com>
2021-08-26 22:12:53 +0530

    Add files via upload

---
+++ PythonProblemSolvingCodeSubmission/ps0.py
@@ -0,0 +1,12 @@
+# Problem Set 0
+# Name : Swarleen Bhamra
+# Time Spent: 2:30 hours (approx)
+
+# To take input of DOB from the user
+s = input ('Enter your date of Birth:\n**')
+
+# To take input of the last name from the user
+q = input ('Enter your last name:\n**')
+
+# To print the last name and DOB of the user
+print(q + " " + s)
